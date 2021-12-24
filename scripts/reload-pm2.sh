4079023fd85fe5686b5fcd8db16f311da8bbb1cf
mujtaba1747 <mujtaba1747@gmail.com>
2021-12-24 15:13:42 +0530

    node err

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 cd ~/aws-codedeploy
 pm2 startOrReload ecosystem.config.js 
